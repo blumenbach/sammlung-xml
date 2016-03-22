ccebbb2d517050c9153dab08287a77178246bbe2
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-03-22 12:51:12 +0100

    first commit

---
+++ test.sh
@@ -0,0 +1,1 @@
+﻿curl --user fedoraAdmin:fedoraAdmin -X PUT --upload-file fuploadtxbolonlineKerndaten10GZGHST00562D05131194545986.JPG -H"Content-Type: image/jpeg" "http://localhost:8080/fcrepo/rest/blum/000001/media/0001_Dazit-Porphyr_(Porfido_rosso_antico)_GZG_0056_00_01.jpg"
